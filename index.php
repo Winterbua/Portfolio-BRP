d01c762b3c3434ddec3cd2578cb5d7014c066ce6
Winter <mayr.manuel29@gmail.com>
2026-05-12 15:43:00 +0200

    First real Commit
    Did mostly experimenting and some light adjusting on the Startpage.

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="manuel mayr">
+    <title>Startseite - Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="header">
+        <a href="index.php">Startseite</a>
+        <a href="blog.php" class="links">Projekte</a>
+        <a href="download.php" class="links">Download</a>
+        <a href="kontakt.php" class="links">Kontakt</a>
+    </div>
+    <div id="body">
+        <h1>Manuel Mayr</h1>
+        <p>Fachschul Absolvent</p>
+        <a href=""></a>
+    </div>
+    <div id="footer">
+        <a href="impressum.php">Impressum</a>
+        <a href="datenschutz.php">Datenschutz</a>
+    </div>
+</body>
+</html>
